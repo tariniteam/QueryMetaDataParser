6729c7feba7aa85d2d855c0807e001fbeb7a5201
Harsha Navalkar <harsha.navalkar@gmail.com>
2023-03-26 02:00:46 +0530

    QMP Portal Config

--- app.py
+++ app.py
@@ -9,6 +9,34 @@ ALLOWED_EXTENSIONS = {'sql'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+def get_query_type ():
+    Jinja_list_QueryType = ['QueryType1', 'QueryType2']
+    return Jinja_list_QueryType
+def get_schema_list ():
+    Jinja_list_Schemas = ['Schema1', 'Schema2']
+    return Jinja_list_Schemas
+
+def get_table_list ():
+    Jinja_list_Tables = ['Table1', 'Table2']
+    return Jinja_list_Tables
+
+def get_column_list ():
+    Jinja_list_Columns = ['Column1', 'Column2']
+    return Jinja_list_Columns
+
+def get_filter_condition_list ():
+    Jinja_list_FilterConditions = ['Filter1', 'Filter2']
+    return Jinja_list_FilterConditions
+
+def get_joins_used ():
+    Jinja_list_JoinsUsed = ['Join1', 'Join2']
+    return Jinja_list_JoinsUsed
+
+def get_all_metadata ():
+    Jinja_list_AllMetadata = ['Metadata1', 'Metadata2']
+    return Jinja_list_AllMetadata
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -57,7 +85,7 @@ def upload_sql_script():
             return render_template("successful_file_upload.html", name=filename)
 
 @app.route('/successful_file_upload', methods=['GET', 'POST' ])
-def display_metadata():
+def type_of_metadata():
     #if request.method == 'POST':
         connection = sq.connect('QMP_DB.db')
         cursor = connection.cursor()
@@ -66,12 +94,45 @@ def display_metadata():
         dropdownlist ="SELECT name from dropdownlist order by name "
         cursor.execute(dropdownlist)
         results = cursor.fetchall()
-        print (results)
+        print ("DB Results", results)
         string_list = [''.join(i) for i in results]
-        print (string_list)
-        return render_template('display_metadata.html', results=string_list)
+        print ("Converted to String", string_list)
+        return render_template('type_of_metadata.html', results=string_list)
 
 
+@app.route('/type_of_metadata', methods=['GET', 'POST'])
+def display_metadata():
+        user_dropdown_selection = request.form.get('dropdownlist_option')
+        print("user_dropdown_selection", user_dropdown_selection)
+
+        Jinja_list_AllMetadata = get_all_metadata()
+        Jinja_list_Columns  = get_column_list()
+        Jinja_list_FilterConditions  = get_filter_condition_list()
+        Jinja_list_JoinsUsed   = get_joins_used()
+        Jinja_list_QueryType   = get_query_type()
+        Jinja_list_Schemas  = get_schema_list()
+        Jinja_list_Tables = get_table_list()
+
+        if  (user_dropdown_selection == 'AllMetadata' ):
+           return render_template('display_metadata.html', Jinja_list=Jinja_list_AllMetadata)
+        elif (user_dropdown_selection == 'Columns' ):
+           return render_template('display_metadata.html', Jinja_list=Jinja_list_Columns)
+        elif (user_dropdown_selection == 'FilterConditions'):
+           return render_template('display_metadata.html', Jinja_list=Jinja_list_FilterConditions)
+        elif (user_dropdown_selection == 'JoinsUsed'):
+           return render_template('display_metadata.html', Jinja_list=Jinja_list_JoinsUsed)
+        elif (user_dropdown_selection == 'QueryType'):
+           return render_template('display_metadata.html', Jinja_list=Jinja_list_QueryType)
+        elif (user_dropdown_selection == 'Schemas'):
+            return render_template('display_metadata.html', Jinja_list=Jinja_list_Schemas)
+        elif (user_dropdown_selection == 'Tables'):
+            return render_template('display_metadata.html', Jinja_list=Jinja_list_Tables)
+        return render_template('display_metadata.html')
+
+@app.route('/display_metadata', methods=['GET', 'POST'])
+def choose_other_metadata_type():
+    if request.method == 'POST':
+        return render_template('type_of_metadata.html')
 
 if __name__ == "__main__":
      app.run(debug=True)
